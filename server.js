f98c3fac85ff1bbd52c6f0ba4e36960c2512d713
shoaibkhan188626 <shoaib.khan.880066@gmail.com>
2024-12-02 06:21:48 +0530

    deletd

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import AppError from "./utils/ErrorHandler.js";
-import gloabalErrorHandler from "./utils/globalErrorHandler.js";
-import authRoutes from "./routes/authRoutes.js";
-import connectDB from "./config/db.js";
 
 dotenv.config({ path: "./config.env" });
 
@@ -16,16 +12,15 @@ router.post("/", () => {
 });
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/v1/auth", authRoutes);
+
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on thid server!`, 404));
 });
 
-app.use(gloabalErrorHandler);
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.listen(PORT, () => {
   console.log(`server running on the port ${PORT}`);
 });
-console.log('kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
+

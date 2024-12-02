1c8a9d09e2155010aced17dfb89170cb89af38e4
shoaibkhan188626 <shoaib.khan.880066@gmail.com>
2024-12-02 06:23:05 +0530

    deleted

--- index.js
+++
@@ -1,6 +0,0 @@
-import connectDB from "./config/db.js";
-import dotenv from "dotenv";
-dotenv.config();
-connectDB();
-console.log(process.env.PORT)
-console.log("-----------------------------")
--- server.js
+++ server.js
@@ -13,14 +13,9 @@ router.post("/", () => {
 app.use(express.json());
 app.use(cookieParser());
 
-app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on thid server!`, 404));
-});
-
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.listen(PORT, () => {
   console.log(`server running on the port ${PORT}`);
 });
-

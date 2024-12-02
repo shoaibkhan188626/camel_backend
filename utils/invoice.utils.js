1cf7c07645222ed6cbec5173efbf47fbed9a1d62
shoaibkhan188626 <shoaib.khan.880066@gmail.com>
2024-12-02 06:20:13 +0530

    deleted several dirs

--- utils/invoice.utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Generates a unique invoice number.
- * @returns {string} A unique invoice number.
- */
-const generateInvoiceNumber = () =>
-  `INV-${Date.now()}-${Math.floor(Math.random() * 10000)}`;
-
-export { generateInvoiceNumber };
